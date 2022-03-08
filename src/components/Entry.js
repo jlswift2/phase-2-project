e417f947c34d58c74a7b126bb406def95d4feaea
Brian Vu <brianvu7@gmail.com>
2022-03-07 21:31:44 -0600

    displayed entries on home. added entry route when clicking entry display on home

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from "react-router-dom";
 
 function Entry() {
+  const { id } = useParams();
+
+  const [entry, setEntry] = useState({});
+  
+  useEffect(() => {
+    fetch(`http://localhost:8002/journals/${id}`)
+    .then(res => res.json())
+    .then(data => setEntry(data));
+  }, [id]);
+
   return (
-    <div>Entry</div>
+    <div>
+      {entry.title} by {entry.author}
+      <br/>
+      {entry.text_body}
+      <br/>
+      mood: {entry.mood}
+    </div>
   )
 }
 
--- src/components/EntryDisplay.js
+++ src/components/EntryDisplay.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import Entry from './Entry'
+import { Link } from "react-router-dom";
 
-function EntryDisplay() {
+function EntryDisplay({ entry }) {
   return (
-    <Entry />
+    <div>
+      <Link to={`/Entry/${entry.id}`}>
+        {entry.title} by {entry.author}
+        <br/>
+        mood: {entry.mood}
+      </Link>
+    </div>
   )
 }
 
