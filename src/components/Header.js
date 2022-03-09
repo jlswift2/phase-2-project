d4eecaac0f2be432ab812b2bea69fcecfffb105f
Vi Vo <aaronvo118@gmail.com>
2022-03-09 08:29:27 -0600

    fixed typos

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,16 @@ import React from 'react'
 import NavBar from './NavBar'
 
 function Header() {
-  return (
 
+
+
+
+
+
+
+
+
+  return (
     <header id="navbar">
       <h1 id="welcome">Welcome!</h1> 
         <NavBar />
