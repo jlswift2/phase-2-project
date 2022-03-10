4cff52cec8ea88b520e57ab0b416e33ac108b2c5
Justin Swift <jlswift2@gmail.com>
2022-03-10 09:31:53 -0600

    Add quick fixes to EntryCard

--- src/components/EntryCard.js
+++ src/components/EntryCard.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-function EntryDisplay({ entry }) {
+function EntryCard({ entry }) {
   return (
       <div className="card">
-      <img id="cardImg" src="https://i.pinimg.com/736x/1b/ef/b5/1befb5723f54627768e1d77642ca4d45.jpg" alt="CATTTTTTT"/>
+      <img id="cardImg" src={entry.img} alt={entry.title} />
         <div className="card_details">
           <span class="tag">{entry.mood}</span>
           <div class="name">{entry.title} By: {entry.author}</div>
           <div class="date">{entry.date}</div>
-          <p>{entry.text_body}</p>
           <Link to={`/Entry/${entry.id}`} style={{ textDecoration: 'none' }}>
             <button>Read more</button>
           </Link>
@@ -18,6 +17,6 @@ function EntryDisplay({ entry }) {
   )
 }
 
-export default EntryDisplay
+export default EntryCard
 
 
